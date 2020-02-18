2baeb75e2b1dc4849706ef7d460469820516988e
Glitch (vortex-register) <none>
2020-02-18 14:15:00 +0000

    🍇🍠 Checkpoint ./events/ready.js:6398621/18

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "VORTEX TEAM; REGİSTER",
+        "VФЯΓΞЖ TEAM; REGİSTER",
 
     ];
 
@@ -15,7 +15,7 @@ var oyun = [
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/fyukas" );
+        client.user.setActivity(oyun[random], "https://www.twitch.tv/" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
