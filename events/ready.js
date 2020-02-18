ef5f8a4fda2e551ff90321e71637e501265d8e12
Glitch (vortex-register) <none>
2020-02-18 14:26:59 +0000

    🐡🌲 Checkpoint ./events/ready.js:6398621/13

--- events/ready.js
+++ events/ready.js
@@ -15,7 +15,7 @@ var oyun = [
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/" );
+        client.user.setActivity(oyun[random], "https://www.twitch.tv/ivortexwinner" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
