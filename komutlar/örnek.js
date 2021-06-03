d22242c426e1cfe269257ccbb921a20bfaec5e9c
Glitch (bos-altyapii-v12) <none>
2021-06-03 14:25:23 +0000

    🌄⛈ Checkpoint ./komutlar/örnek.js:7473276/476

--- komutlar/örnek.js
+++ komutlar/örnek.js
@@ -1,25 +1,12 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
- const csg = client.guilds.fetch()
- const csm = client.users.fetch()
-  
- const csmsg = message.channel.send("Gönderim Başladı!...").then(cs => cs.id)
- 
- client.users.cache.map(cs => {
-   
-      cs.send().catch(async err => {
-                  return await console.log(
-                    `${message.author.tag}, **İsimli Üyenin DM'si Kapalı Diye Mesaj Gönderilemedi!!**`
-                  );
-              });
-   
- })
+message.channel.send("https://discord.gg/6XGqdgE")
 };
 exports.conf = {
   aliases: []
 };
 
 exports.help = {
-  name: "dm"
+  name: "link"
 };
