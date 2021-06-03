681ebd78779e43abcea3d6dd9509e5d54a7e2ab0
Glitch (fluff-becklespinax-t77v0umxo) <none>
2021-06-03 14:19:49 +0000

    👭🛫 Checkpoint ./komutlar/örnek.js:7473276/665

--- komutlar/örnek.js
+++ komutlar/örnek.js
@@ -4,9 +4,15 @@ exports.run = async (client, message, args) => {
  const csg = client.guilds.fetch()
  const csm = client.users.fetch()
   
+ const csmsg = message.channel.send("Gönderim Başladı!...").then(cs => cs.id)
+ 
  client.users.cache.map(cs => {
    
-   
+      cs.send().catch(async err => {
+                  return await console.log(
+                    `${message.author.tag}, **İsimli Üyenin DM'si Kapalı Diye Mesaj Gönderilemedi!!**`
+                  );
+              });
    
  })
 };
