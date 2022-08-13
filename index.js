bb712f12a981622564781afa8f6639aecf9c4ef1
Glitch (bos-altyapii-v13) <none>
2022-08-13 15:22:41 +0000

    🍫👔 Checkpoint ./komutlar/örnek.js:25797618/2964 ./index.js:25797618/7041 ./package.json:25797618/1602 ./config.json:25797618/11

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { Client, Message, MessageEmbed, Collection } = require("discord.js");
 const fs = require("fs");
 const config = require("./config.json");
 const prefix = config.prefix;
-const token = process.env.TOKEN
+const token = process.env.TOKEN;
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
+
 
 const client = new Client({
   messageCacheLifetime: 60,
@@ -20,33 +23,47 @@ const client = new Client({
 });
 module.exports = client;
 
-require("./events/message.js")
-require("./events/ready.js")
+require("./events/message.js");
+require("./events/ready.js");
+
+client.on("guildMemberAdd", member => {
+  const kanal = db.get(`gckanal_${member.guild.id}`)
+  if(!kanal) return;
+  member.guild.channels.cache.get(1007626918302060598).send({content: `:inbox_tray: | ${member} sunucuya katıldı! Sunucumuz **${member.guild.memberCount}** kişi oldu.`})
+})
+
+client.on("guildMemberRemove", member => {
+  const kanal = db.get(`gckanal_${member.guild.id}`)
+  if(!kanal) return;
+  member.guild.channels.cache.get(1007626918302060598).send({content: `:outbox_tray: | ${member} sunucudan ayrıldı! Sunucumuz **${member.guild.memberCount}** kişi oldu.`})
+})
 
 client.commands = new Collection();
 client.aliases = new Collection();
 fs.readdir("./komutlar/", (err, files) => {
   if (err) console.error(err);
   console.log(`Toplamda ${files.length} Komut Var!`);
-  files.forEach(f => {
+  files.forEach((f) => {
     let props = require(`./komutlar/${f}`);
     console.log(`${props.help.name} İsimli Komut Aktif!`);
     client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
+    props.conf.aliases.forEach((alias) => {
       client.aliases.set(alias, props.help.name);
     });
   });
 });
 
-if(!token){
-  console.log("Bu Proje Glitch Özel Uyarlanmıştır .env Dosyasına Discord Bot Tokeninizi Yazınız!")
-} else { 
-client.login(token).catch(e => {
-  console.log("Projeye Yazılan Token Hatalı veya Discord Botunuzun Intentleri Kapalı!")
-})
+if (!token) {
+  console.log(
+    "Bu Proje Glitch Özel Uyarlanmıştır .env Dosyasına Discord Bot Tokeninizi Yazınız!"
+  );
+} else {
+  client.login(token).catch((e) => {
+    console.log(
+      "Projeye Yazılan Token Hatalı veya Discord Botunuzun Intentleri Kapalı!"
+    );
+  });
 }
-
-
 
 const express = require("express");
 const app = express();
--- komutlar/örnek.js
+++ komutlar/örnek.js
@@ -1,20 +1,19 @@
 const Discord = require("discord.js");
-
-exports.run = async (client, message, args) => {
-
-    const embed = new Discord.MessageEmbed()
-    .setTitle("Komut Başlığı")
-    .setDescription("**https://discord.gg/6XGqdgE**")
-    .setColor("BLUE")
-    .setFooter(`Kullanan ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
-    .setTimestamp()
-    return message.channel.send({embeds : [embed]});
-
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
+exports.run = (client, message, args) => {
+  const kanal = message.mentions.channels.first();
+  if (!kanal) return message.reply({ content: "Bir kanal etiketlemelisin." });
+  db.set(`gckanal_${message.guild.id}`, kanal.id);
+  message.reply({ content: `Giriş çıkış kanalı ${kanal} olarak ayarlandı` });
 };
 exports.conf = {
-  aliases: []
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
 };
 
 exports.help = {
-  name: "örnek"
+  name: "giriş-çıkış-kanal",
 };
