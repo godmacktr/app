ce89846f512e17157e44fb6a4c8daa1ca3d895d8
Glitch (poised-magic-hollyhock) <none>
2022-08-16 14:57:42 +0000

    🐴🌗 Checkpoint ./src/commands/ping.js:25797618/926

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,12 +1,14 @@
 const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
+
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Pong!"),
-    run: async (client, interaction) => {
-      interaction.reply(`Pong 🏓`)
+    run: async (client, interaction, message) => {
+      interaction.reply(`**:ping_pong:Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms.:ping_pong:**`);
       // komuta seçenekler eklemek istersen guide: https://discordjs.guide/interactions/slash-commands.html#options
-    }
- };
+ }
+}
