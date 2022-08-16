33f12c3e617e7b3758db06d72b8006c9d3b590de
Glitch (poised-magic-hollyhock) <none>
2022-08-16 15:20:48 +0000

    🏆🙈 Checkpoint ./src/commands/ping.js:25797618/1684

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,14 +1,26 @@
 const { EmbedBuilder, PermissionsBitField } = require("discord.js");
-const { SlashCommandBuilder } = require("@discordjs/builders");
-
 
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Pong!"),
-    run: async (client, interaction, ) => {   
-      interaction.reply(":ping_pong: \nBot Gecikmesi  \`${date.now() - interaction.createdTimestamp}`\, API Gecikmesi: ${Math.round(client.ws.ping)}ms.:ping_pong:");
-      // komuta seçenekler eklemek istersen guide: https://discordjs.guide/interactions/slash-commands.html#options
- }
-}
+    name: "ping",
+    description: "Botun Ping hakkında bilgi vermesini sağlar",
+    permissions: "0x0000000000000800",
+    options: [],
+    run: async (client, interaction) => {
+
+        const start = Date.now();
+        const embed = new EmbedBuilder()
+            .setColor('007fff')
+            .setTitle(client.user.username + " - Ping!")
+            .setThumbnail(client.user.displayAvatarURL())
+            .addFields([
+                { name: `Mesaj Ping`, value: `\`${Date.now() - start}ms\` 🛰️` },
+                { name: `Mesaj Gecikmesi`, value: `\`${Date.now() - start}ms\` 🛰️` },
+                { name: `API Gecikmesi`, value: `\`${Math.round(client.ws.ping)}ms\` 🛰️` }
+            ])
+            .setTimestamp()
+            .setFooter({ text: `Asura` })
+        interaction.reply({ embeds: [embed] }).catch(e => { });
+
+    },
+};
