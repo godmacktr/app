7231da76a5e3fbc8e9c9a1e8dc8263ea54fdb04d
Glitch (poised-magic-hollyhock) <none>
2022-08-16 12:33:41 +0000

    🌶🌘 Checkpoint ./index.js:25797618/715

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { Routes } = require('discord-api-types/v10');
 
 client.commands = new Collection()
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.token);
 
 const log = l => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${l}`) };
 
@@ -33,7 +33,22 @@ client.on("ready", async () => {
     log(`${client.user.username} Aktif Edildi!`);
 })
 
+client.on("guildMemberAdd", (member) => {
+ try {
+const exampleEmbed = new EmbedBuilder()
+	.setColor('2F3136')
+	.setTitle(`Crex'e Hoş Geldiniz!`)
+	.setDescription(`Seni aramızda görmek ne güzel ${member}! Kurallarımıza uymayı ve keyif almayı unutmayın!`)
+  .setThumbnail(member.user.avatarURL({ dynamic: true }))
+	.setFooter({ text: `Guild Member Count: #${member.guild.memberCount}`, iconURL: 'https://thumbs.dreamstime.com/b/letter-logo-design-simple-modern-logo-design-letter-very-simple-black-background-color-183193944.jpg' });
 
+  member.guild.channels.cache
+      .get("911590826931535933")
+      .send({ embeds: [exampleEmbed] });
+} catch(error) {
+  console.log("error =>", error);
+}
+});
 
 //event-handler
 const eventFiles = readdirSync('./src/events').filter(file => file.endsWith('.js'));
