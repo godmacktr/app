e8e05de9787ab5b47319b0f35fad29d464098309
Glitch (poised-magic-hollyhock) <none>
2022-08-16 11:58:40 +0000

    🦄⛰ Checkpoint ./src/events/ready.js:25797618/17

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,5 +4,5 @@ module.exports = {
 	once: true,
 	execute(client) {
     let activities = [ `Somer Şef İfşa İzliyor`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
+    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Watching }), 22000);
 }};
