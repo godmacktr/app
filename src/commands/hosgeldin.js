8b230d0d8390f8e674d0d506c86f4e5e9cf46133
Glitch (poised-magic-hollyhock) <none>
2022-08-16 17:19:30 +0000

    🍦💧 Checkpoint ./src/commands/ban.js:25797618/583 ./src/commands/hosgeldin.js:25797618/570

--- src/commands/hosgeldin.js
+++ src/commands/hosgeldin.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js')
+const { PermissionsBitField } = require('discord.js') 
 const db = require('orio.db')
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
@@ -6,23 +6,23 @@ module.exports = {
  data: new SlashCommandBuilder()
 .setName('hgayarla')
 .setDescription('Hosgeldin Kanalını ayarlar'),
-run: async (client, interaction ,args) => {
+run: async (client, member ,args) => {
 
-if(!interaction.member.permissions.has("ManageGuild")) return interaction.reply("YETERSİZ YETKİ! YETKİN YOK")
+if (member.permissions.has("ManageGuild")) return member.reply("YETERSİZ YETKİ! YETKİN YOK")
 
-if(!args[0]) return interaction.reply("**Hatalı Kullanım❌\nAyarlamak İçin: !hg log-ayarla #kanal\nSıfırlamak İçin: !hg-bb log-sıfırla**")
-  let kanal = interaction.mentions.channels.first()
+if(!args[0]) return member.reply("**Hatalı Kullanım❌\nAyarlamak İçin: !hg log-ayarla #kanal\nSıfırlamak İçin: !hg-bb log-sıfırla**")
+  let kanal = member.mentions.channels.first()
 
   if(args[0] == 'log-ayarla') {
-  db.set(`cshg.${interaction.guild.id}`, kanal.id)
-if(!kanal) return interaction.reply("**Bir Kanal Belirtmedin!**")
-  interaction.channel.send(`**Hoş Geldin  Kanalı Başarıyla \`${kanal.name}\` Olarak Ayarlandı!**`)
+  db.set(`cshg.${member.guild.id}`, kanal.id)
+if(!kanal) return member.reply("**Bir Kanal Belirtmedin!**")
+  member.channel.send(`**Hoş Geldin  Kanalı Başarıyla \`${kanal.name}\` Olarak Ayarlandı!**`)
     
   }
 
   if(args[0] == 'log-sıfırla') {
-  db.delete(`cshg.${interaction.guild.id}`)
-  interaction.channel.send(`**Sistem Sıfırlandı!**`)
+  db.delete(`cshg.${member.guild.id}`)
+  member.channel.send(`**Sistem Sıfırlandı!**`)
  
   }
 }
