07b97dd41868219da3f8aa8992c3053b83f0db8d
Glitch (poised-magic-hollyhock) <none>
2022-08-18 16:37:23 +0000

    👟🐏 Checkpoint ./src/commands/kac-cm.js:25797618/426

--- src/commands/kac-cm.js
+++ src/commands/kac-cm.js
@@ -3,23 +3,23 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("kac-cm")
-    .setDescription("Kac Cm Olduğunu Söyler"),
-  run: async (client, interaction, args) => {
+    .setName('kac-cm')
+    .setDescription('Kac Cm Olduğunu Söyler'),
+  run: async (client, interaction ) => {
     interaction.channel
-      .send("Hemen Diyorum Abi 1 Saniye..")
+      .send('Hemen Diyorum Abi 1 Saniye..')
       .then((interaction2) => {
         var espriler = [
-          " **Senin Malafatın  `18CM`  :eggplant: ",
-          "**Senin Malafatın  `11CM`  :eggplant:",
-          "**Senin Malafatın `32CM`   :eggplant:",
-          "**Senin Malafatın  `35CM`  :eggplant:",
-          "**Senin Malafatın  **`8CM`   :eggplant:",
-          "**Senin Malafatın  **`65CM`   :eggplant:",
-          "**Senin Malafatın **`5CM`   :eggplant:",
-          "**Senin Malafatın **`31CM`   :eggplant:",
-          "**Senin Malafatın ** `14CM`  :eggplant:",
-          "**Senin Malafatın ** `1CM`  :eggplant:",
+          '**Senin Malafatın** `18CM` :eggplant:',
+          '**Senin Malafatın** `11CM` :eggplant:',
+          '**Senin Malafatın** `32CM` :eggplant:',
+          '**Senin Malafatın** `35CM` :eggplant:',
+          '**Senin Malafatın** `8CM`  :eggplant:',
+          '**Senin Malafatın** `65CM` :eggplant:',
+          '**Senin Malafatın** `5CM`  :eggplant:',
+          '**Senin Malafatın** `31CM` :eggplant:',
+          '**Senin Malafatın** `14CM` :eggplant:',
+          '**Senin Malafatın** `1CM`  :eggplant:',
         ];
         var espri = espriler[Math.floor(Math.random() * espriler.length)];
         interaction2.edit(`${espri}`);
