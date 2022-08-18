708fa02d2287b3a720e50b09ed1ee1b4c493bb99
Glitch (poised-magic-hollyhock) <none>
2022-08-18 16:25:18 +0000

    💇💶 Checkpoint ./src/commands/ping.js:25797618/2033 ./src/commands/kac-cm.js:25797618/2033

--- src/commands/kac-cm.js
+++ src/commands/kac-cm.js
@@ -1,14 +1,28 @@
-const { EmbedBuilder } = require("discord.js");
+const Discord = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+
 module.exports = {
-data: new SlashCommandBuilder()
- .setName("kac-cm")
- .setDescription("Kac Cm Olduğunu Söyler"),
- run: async (client, interaction, args) => { 
-interaction.channel.send('Hemen Diyorum Abi 1 Saniye..').then(interaction2 => {
-var espriler = [' **Senin Malafatın  `18CM` ** :eggplant: ' ,'**Senin Malafatın  `11CM` ** :eggplant:' ,'**Senin Malafatın `32CM`  ** :eggplant:' ,'**Senin Malafatın  `35CM` ** :eggplant:' ,'**Senin Malafatın  `8CM`  ** :eggplant:' ,'**Senin Malafatın  `65CM`  ** :eggplant:' ,'**Senin Malafatın `5CM`  ** :eggplant:' ,'**Senin Malafatın `31CM`  ** :eggplant:' ,'**Senin Malafatın  `14CM` ** :eggplant:' ,'**Senin Malafatın  `1CM` ** :eggplant:'];
-var espri = espriler[Math.floor(Math.random() * espriler.length)];
-interaction2.edit(`${espri}`);
-})
-},
+  data: new SlashCommandBuilder()
+    .setName("kac-cm")
+    .setDescription("Kac Cm Olduğunu Söyler"),
+  run: async (client, interaction, args) => {
+    interaction.channel
+      .send("Hemen Diyorum Abi 1 Saniye..")
+      .then((interaction2) => {
+        var espriler = [
+          " **Senin Malafatın  `18CM`  :eggplant: ",
+          "**Senin Malafatın  `11CM`  :eggplant:",
+          "**Senin Malafatın `32CM`   :eggplant:",
+          "**Senin Malafatın  `35CM`  :eggplant:",
+          "**Senin Malafatın  **`8CM`   :eggplant:",
+          "**Senin Malafatın  **`65CM`   :eggplant:",
+          "**Senin Malafatın **`5CM`   :eggplant:",
+          "**Senin Malafatın **`31CM`   :eggplant:",
+          "**Senin Malafatın ** `14CM`  :eggplant:",
+          "**Senin Malafatın ** `1CM`  :eggplant:",
+        ];
+        var espri = espriler[Math.floor(Math.random() * espriler.length)];
+        interaction2.edit(`${espri}`);
+      });
+  },
 };
--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,33 +1,15 @@
-const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Botun Pingi Hakkında bilgi verir."),
-    run: async (client, interaction) => {
-      interaction.reply(`\nBot Gecikmesi: \`${Date.now() - interaction.createdTimestamp}ms\`, API Gecikmesi: \`${Math.round(client.ws.ping)}ms\``)
-      // komuta seçenekler eklemek istersen guide: https://discordjs.guide/interactions/slash-commands.html#options
-    }
- };
-
-const Discord = require("discord.js");
-
-module.exports = {
-    slash: true,
-    name: ['ping'],  
-    description: 'Botun Pingini Görürsünüz.', 
-    option: [],
-	async execute(client, interaction) {
-	
-    interaction.reply({
-      embeds: [{
-        Color: 'Gold',
-      image: {
-	url: 'https://dummyimage.com/2000x500/33363c/ffffff&text='+ client.ws.ping +'%20MS',
-	},
-        
-      }]
-    })
-  },
-};
+data: new SlashCommandBuilder()
+    .setName('ping')
+   .setDescription  ('Botun Pingini Görürsünüz.'), 
+	run :async (client, interaction) => {
+  const cse = new EmbedBuilder()
+        .setColor('Gold')
+      .setImage('https://dummyimage.com/2000x500/33363c/ffffff&text='+ client.ws.ping +'%20MS')
+interaction.reply({ embeds: [cse] }) 
+    
+}
+}  
