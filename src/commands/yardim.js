d9e69849747937c70d7283537bd8249e2c099a40
Glitch (poised-magic-hollyhock) <none>
2022-08-19 12:47:37 +0000

    👾🏜 Checkpoint ./src/commands/yardim.js:25797618/10927

--- src/commands/yardim.js
+++ src/commands/yardim.js
@@ -1,37 +1,17 @@
-const Discord = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders"); 
+
 module.exports = {
  data: new SlashCommandBuilder()
-    .setName("yardım")
-    .setDescription("Botun Yardım Menüsü"),
-	run: async (client, interaction) => { 
-	    interaction.reply({
-
-      embeds: [
-
-        {
-
-          title: `Asura | Yardım`,
-          color: 0xFFD700,
-          fields: [
-
-            { name: "Hava Durumu", value: "Yazdığınız bölgenin hava durumunu verir.", inline: true },
-
-            { name: "Hgayarla", value: "Hosgeldin kanalını ayarlar.", inline: true },
-
-            { name: "Ping", value: "Botun pingi hakkında bilgi verir.", inline: true },
-            
-            { name: "Yardım", value: "Bottaki komutları gösterir.", inline: true },
-            
-            { name: "Zar-at", value: "1-6 arası zar atar.", inline: true },
-             
-            { name: "Yapımcı", value: "Botun yapımcısı söyler.", inline: true },
-               
-                 
-
-          ]
-        }
-      ]
-    })
-  },
-};
+.setName("")
+.setDescription("")
+run: async (client, interaction) => { 
+const cse = new EmbedBuilder()
+.setTitle(client.user.username+" Yardım Menüsü")
+.setColor("Blue")
+.setThumbnail(client.user.avatarURL())
+.setDescription(`KOMUTLARINIZI VS YAZARAK AÇIKLAYA BILIRSINIZ!`)
+.setFooter({text:"Made By. Ege'#0001"})
+.setTimestamp()
+message.channel.send({ embeds: [cse] })
+}
