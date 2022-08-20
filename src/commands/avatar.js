2650c541508bd5e622a6c7b21263f647a2e2045a
Glitch (poised-magic-hollyhock) <none>
2022-08-20 17:53:49 +0000

    🗽😹 Checkpoint ./src/commands/rol-ver.js:25797618/428 ./src/commands/avatar.js:25797618/1189

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -5,15 +5,16 @@ module.exports = {
 data: new SlashCommandBuilder() 
   .setName('avatar')
   .setDescription('Kullanıcının Avatarına bakarsınız.')
-.addUserOption(option =>
-		option.setName('id')
-    .setDescription('kullanıcının idsi')
-			.setRequired(true)),
-  run: async(member, client,  interaction) => {
-    let user = interaction.author;
+.addSubcommand(subcommand =>
+		subcommand
+		.setName('id')
+    .setDescription('kullanıcın idsi')
+		.addUserOption(option => option.setName('target').setDescription('kullanıcı'))),
+  run: async(client,  interaction) => {
+    let user =  interaction.options.getUser() ||  interaction.author;
     const cse = new EmbedBuilder()
-    .setDescription(`**[PNG](${member.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "png")}) | [JPG](${member.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "jpg")}) | [WEBP](${member.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "webp")}) | [GIF](${member.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "gif")})**`)
-    .setImage(member.avatarURL({ dynamic: true, size: 1024 }))
+    .setDescription(`**[PNG](${user.author.displayAvatarURL({ dynamic: true, size: 1024 }).replace("webp", "png")}) | [JPG](${user.author.displayAvatarURL({ dynamic: true, size: 1024 }).replace("webp", "jpg")}) | [WEBP](${user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "webp")}) | [GIF](${user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "gif")})**`)
+    .setImage(user.author.displayAvatarURL({ dynamic: true, size: 1024 }))
     .setColor("#0099ff")
     return  interaction.reply({ embeds: [cse]}).catch(err => {})
 
--- src/commands/rol-ver.js
+++ src/commands/rol-ver.js
@@ -8,16 +8,20 @@ data: new SlashCommandBuilder()
 .setDescription("Belirlediğiniz kişiye rol verirsiniz")
   .addSubcommand(subcommand =>
 		subcommand
-			.setName('user')
-			.setDescription('Info about a user')
-			.addUserOption(option => option.setName('kullanıcı').setDescription('kullanıcı'))),
-  
+			.setName('rol')
+			.setDescription('rol')
+			.addUserOption(option => option.setName('target').setDescription('kullanıcı')))
+  .addSubcommand(subcommand =>
+		subcommand
+			.setName('server')
+			.setDescription('Info about the server'))
+      .addRoleOption(option => option.setName('role').setDescription('verilcek rol')),
     run: async(client, interaction) => {
 if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageRoles)) 
   return interaction.reply(
     "Rolleri Yönet Yetkiniz Bulunmamakta.")
-let user = interaction.mentions.user.first();
-let rol = interaction.mentions.roles.first(1010506971176243242);
+let user = interaction.options.getUser();
+let rol = interaction.options.getRole();
 if(!user) return interaction.reply("Lütfen Rolün Verileceği Kişiyi Belirtiniz.")
 if(!rol) return interaction.reply("Lütfen Verilecek Rolü Belirtiniz.")
 //norex
