bac87bf4a29930c3751efde7ec3dda3f4ca314ae
Glitch (poised-magic-hollyhock) <none>
2022-08-20 20:41:09 +0000

    ⛽️🐞 Checkpoint ./src/commands/avatar.js:25797618/2368

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -6,12 +6,12 @@ data: new SlashCommandBuilder()
   .setName('avatar')
   .setDescription('Kullanıcının Avatarına bakarsınız.')
 .addSubcommand(subcommand =>
-		subcommand
-		.setName('kullanıcı')
+        subcommand
+        .setName('kullanıcı')
     .setDescription('kullanıcı')
-		.addUserOption(option => option.setName('user').setDescription('kullanıcı'))),
+        .addUserOption(option => option.setName('user').setDescription('kullanıcı'))),
   run: async(client,  interaction, message) => {
-    let user = interaction.author; 
+    let user = interaction.author => interaction.user
     const cse = new EmbedBuilder()
     .setDescription(`**[PNG](${interaction.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "png")}) | [JPG](${interaction.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "jpg")}) | [WEBP](${interaction.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "webp")}) | [GIF](${interaction.user.avatarURL({ dynamic: true, size: 1024 }).replace("webp", "gif")})**`)
     .setImage(interaction.user.avatarURL({ dynamic: true, size: 1024 }))
