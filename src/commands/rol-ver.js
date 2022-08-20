28ba90cb62f8eb98328204c58426dcf43cbe1b3b
Glitch (poised-magic-hollyhock) <none>
2022-08-20 12:24:29 +0000

    🏄👣 Checkpoint ./src/commands/rol-ver.js:25797618/342

--- src/commands/rol-ver.js
+++ src/commands/rol-ver.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, ApplicationCommandType, ApplicationCommandOptionType } = require("discord.js");
+const { EmbedBuilder, ApplicationCommandType, ApplicationCommandOptionType, PermissionsBitField } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 
@@ -6,18 +6,19 @@ module.exports = {
 data: new SlashCommandBuilder()
 .setName("rol-ver")
 .setDescription("Belirlediğiniz kişiye rol verirsiniz"),
-    run: async(client, message, args) => {
-if(!message.member.permissions.has("MANAGE_ApplicationCommandOptionType.RoleS")) return message.reply("Rolleri Yönet Yetkiniz Bulunmamakta.")
-let user = message.mentions.users.first();
-let rol = message.mentions.roles.first();
-if(!user) return message.reply("Lütfen Rolün Verileceği Kişiyi Belirtiniz.")
-if(!rol) return message.reply("Lütfen Verilecek Rolü Belirtiniz.")
+    run: async(client, interaction) => {
+if(!interaction.interaction.permissions.has("MANAGE_ROLES")) return interaction.reply("Rolleri Yönet Yetkiniz Bulunmamakta.")
+let user = interaction.mentions.users.first();
+let rol = interaction.mentions.roles.first();
+if(!user) return interaction.reply("Lütfen Rolün Verileceği Kişiyi Belirtiniz.")
+if(!rol) return interaction.reply("Lütfen Verilecek Rolü Belirtiniz.")
 //norex
-message.guild.members.cache.get(user.id).roles.add(rol)
+interaction.guild.members.cache.get(user.id).roles.add(rol)
 const cse = new EmbedBuilder()
 .setColor("Gold")
 .setAuthor("Rol Verdin Sana Knk .D")
 .setDescription(`${user}, isimli kişiye ${rol} isimli rol verildi.`)
 
-message.reply({embeds:[embed]})
+interaction.reply({embeds:[cse]})
  }
+  }
