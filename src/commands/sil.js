bba7d00643b9599e73e33342edd602e8f721f00b
Glitch (poised-magic-hollyhock) <none>
2022-08-24 13:29:16 +0000

    🐞🍢 Checkpoint ./src/commands/sil.js:25797618/311

--- src/commands/sil.js
+++ src/commands/sil.js
@@ -2,11 +2,15 @@ const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
 data: new SlashCommandBuilder()
-    .setName('ping')
-   .setDescription  ('Botun Pingini Görürsünüz.'), 
+    .setName('sil')
+   .setDescription('Mesaj 1 ve 100 arası mesaj silersiniz.'),
+  .addStringOption(option =>
+		option.setName('category')
+			.setDescription('The gif category')
 run: async (client, interaction, args) => {
 if(!interaction.member.permissions.has("0x0000000000002000")) return interaction.reply("Yetersiz Yetki! Gereken **Mesajları Yönet**").catch(err => {})
-
+  
+let user = interaction.options.getUser("kullanıcı") || interaction.user
 let sayı = args[0]
 if(sayı < 1 || sayı > 100) return interaction.reply({ content: "1 ile 100 arasında bir sayı belirtin." }).catch(err => {})
 await interaction.channel.interactions.fetch({limit: sayı})
@@ -19,7 +23,7 @@ return interaction.reply({ content: "Mesaj silinemedi. Çok eski mesajlar yada y
 const cse = new EmbedBuilder()
 .setTitle("Mesajlar silindi.")
 .setDescription(`${sayı} mesaj silindi.`)
-.setFooter({text: `${interaction.user.username} tarafından istendi.` })
+.setFooter({text: `${user.tag} tarafından istendi.` })
 .setColor("#0099ff")
 interaction.reply({ embeds: [cse]}).then(async msg => {
 
