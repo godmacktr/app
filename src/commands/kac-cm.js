628b61bf2f2b69eb4b2ba8cdca814f603195a456
Glitch (poised-magic-hollyhock) <none>
2022-08-24 15:26:12 +0000

    🌘🐩 Checkpoint ./src/commands/kac-cm.js:25797618/1878

--- src/commands/kac-cm.js
+++ src/commands/kac-cm.js
@@ -6,18 +6,18 @@ module.exports = {
     .setDescription('Kac Cm Olduğunu Söyler'),
   run: async (client, interaction) => {
         var espriler = [
-          `**  Senin Malafatın** \`18CM\` :eggplant:`,
-          `**  Senin Malafatın** \`11CM\` :eggplant:`,
-          `**  Senin Malafatın** \`32CM\` :eggplant:`,
-          `**  Senin Malafatın** \`35CM\` :eggplant:`,
-          `**  Senin Malafatın** \`8CM\`  :eggplant:`,
-          `**  Senin Malafatın** \`65CM\` :eggplant:`,
-          `**  Senin Malafatın** \`5CM\`  :eggplant:`,
-          `**  Senin Malafatın** \`31CM\` :eggplant:`,
-          `**  Senin Malafatın** \`14CM\` :eggplant:`,
-          `**  Senin Malafatın** \`1CM\`  :eggplant:`,
+          `**Senin Malafatın** \`18CM\` :eggplant:`,
+          `**Senin Malafatın** \`11CM\` :eggplant:`,
+          `**Senin Malafatın** \`32CM\` :eggplant:`,
+          `**Senin Malafatın** \`35CM\` :eggplant:`,
+          `**Senin Malafatın** \`8CM\`  :eggplant:`,
+          `**Senin Malafatın** \`65CM\` :eggplant:`,
+          `**Senin Malafatın** \`5CM\`  :eggplant:`,
+          `**Senin Malafatın** \`31CM\` :eggplant:`,
+          `**Senin Malafatın** \`14CM\` :eggplant:`,
+          `**Senin Malafatın** \`1CM\`  :eggplant:`,
         ];
         var espri = espriler[Math.floor(Math.random() * espriler.length)];
-     interaction.channel.send
+     interaction.reply({content: `${espri}`})
       }
   }
