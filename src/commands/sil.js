2176124703f7f367dac0bb496ede3fd993286acb
Glitch (poised-magic-hollyhock) <none>
2022-08-24 14:28:04 +0000

    👣🔮 Checkpoint ./src/commands/sil.js:25797618/3879

--- src/commands/sil.js
+++ src/commands/sil.js
@@ -1,37 +1,50 @@
-const { EmbedBuilder } = require("discord.js");
+const { Client, EmbedBuilder, ApplicationCommandType, ApplicationCommandOptionType, PermissionsBitField } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
-data: new SlashCommandBuilder()
-    .setName('sil')
-   .setDescription('Mesaj 1 ve 100 arası mesaj silersiniz.')
-  .addNumberOption(option =>
-			option.setName('sayı')
-			.setDescription('Info about a user')),
-run: async (client, interaction) => {
-if(!interaction.member.permissions.has("0x0000000000002000")) return interaction.reply("Yetersiz Yetki! Gereken **Mesajları Yönet**").catch(err => {})
-  
-let user = interaction.options.getUser("kullanıcı") || interaction.user
-let sayı = interaction.options.getNumber('num');
-if(sayı < 1 || sayı > 100) return interaction.reply({ content: "1 ile 100 arasında bir sayı belirtin." }).catch(err => {})
-await interaction.channel.interactions.fetch({limit: sayı})
-.then(interactions => interaction.channel.bulkDelete(interactions))
-.catch(err => {
-return interaction.reply({ content: "Mesaj silinemedi. Çok eski mesajlar yada yetkim yetersiz." }).catch(err => {})
-})
-  
+  data: new SlashCommandBuilder()
+    .setName("sil")
+    .setDescription("Mesaj 1 ve 100 arası mesaj silersiniz.")
+    .addNumberOption((option) =>
+      option
+        .setName("sayı")
+        .setDescription("sayı belirle")
+        .setRequired(true)
+    ),
+  run: async (client, interaction) => {
+    let user = interaction.options.getUser("kullanıcı") || interaction.user
+    let sayı = interaction.options.getNumber("num");
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages))
+      return interaction
+        .reply("Yetersiz Yetki! Gereken **Mesajları Yönet**")
+        .catch((err) => {});
+    if (sayı < 1 || sayı > 100)
+      return interaction
+        .reply({ content: "1 ile 100 arasında bir sayı belirtin." })
+        .catch((err) => {});
+    await interaction.guild.channel
+      .fetch({ limit: sayı })
+      .then((interactions) => interaction.channel.bulkDelete(interactions))
+      .catch((err) => {
+        return interaction
+          .reply({
+            content:
+              "Mesaj silinemedi. Çok eski mesajlar yada yetkim yetersiz.",
+          })
+          .catch((err) => {});
+      });
 
-const cse = new EmbedBuilder()
-.setTitle("Mesajlar silindi.")
-.setDescription(`${sayı} mesaj silindi.`)
-.setFooter({text: `${user.tag} tarafından istendi.` })
-.setColor("#0099ff")
-interaction.reply({ embeds: [cse]}).then(async msg => {
-
-setTimeout(async() => {
-await msg.delete().catch(e => {})
-}, 10000)
-
-}).catch(err => {})
-
-}
-}
+    const cse = new EmbedBuilder()
+      .setTitle("Mesajlar silindi.")
+      .setDescription(`${sayı} mesaj silindi.`)
+      .setFooter({ text: `${user.tag} tarafından istendi.` })
+      .setColor("#0099ff");
+    interaction
+      .reply({ embeds: [cse] })
+      .then(async (msg) => {
+        setTimeout(async () => {
+          await msg.delete().catch((e) => {});
+        }, 10000);
+      })
+      .catch((err) => {});
+  },
+};
